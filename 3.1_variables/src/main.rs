477000b9be9de3448c5fd2c91822877e0c1d5058
Vinicius Freitas <viniciusscfjr@gmail.com>
2023-10-01 13:28:00 -0300

    chore: 3.1 finished

--- 3.1_variables/src/main.rs
+++ 3.1_variables/src/main.rs
@@ -1,3 +1,41 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Mutable Variable");
+    mutable_variable();
+    println!("-----------------");
+
+    println!("Shadowing");
+    shadowing();
+    println!("-----------------");
+    
+    println!("Reassign Variable");
+    reassign_variable();
+    println!("-----------------");
+}
+
+fn mutable_variable() {
+    let mut x = 5;
+    println!("The value of x is: {x}");
+    x = 6;
+    println!("The value of x is: {x}");
+}
+
+fn shadowing() {
+    let x = 5;
+
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
+}
+
+fn reassign_variable() {
+    let spaces = "   ";
+    println!("The value of spaces is: {spaces}");
+
+    let spaces = spaces.len();
+    println!("The value of spaces is: {spaces}");
 }
--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-#### Creating new exercise
-```
+# Rust-Book exericises
+
+## Creating new exercise
+
+```bash
 cargo new {CHAPTER_NUMBER}_{PROJECT_NAME} --name {PROJECT_NAME}
 ```
 
-#### Running exercise
-```
+## Running exercise
+
+```bash
 cargo run --manifest-path=.\{CHAPTER_NUMBER}_{PROJECT_NAME}\Cargo.toml
+```
+
+```bash
+run test
 ```
